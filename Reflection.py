28a9b221f49205789f3ba57a6689268d85adbc17
Fridah Cheboi <114306628+FRIDAHCHEBOI01@users.noreply.github.com>
2023-02-28 19:56:21 +0000

    Add files via upload

---
+++ Reflection.py
@@ -0,0 +1,81 @@
+'''
+Does my language shape how I think?
+Well yes to a certain degree it does. However it is important to note that our
+thinking as people is complex and truly systemic. Hence, there are several
+interconnected factors that contribute to shape how we think. Our language is
+only a fraction of it. Some of these factors include, religion, exposure,
+beliefs, culture, all dogmas, parents and society, financial standing and reading.
+The language of space, is language that is used to describe space. When I started
+reading this I thought space, azin outer space, the planets and all.
+Our languages influence how we interact with the world. As the article suggests,
+certain geographical languages have us paying much keener attention to detail
+more than others. "Everyday communication in a geographic language provides the
+most intense imaginable drilling in geographic orientation.", says the author.
+4 paragraphs: 
+a.What surprised you most in reading the excerpt?
+
+
+b.>>>What is your primary native language (besides English?)
+My mother tongue is Kiswahili. It is Kenya's National language and is spoken
+widely in Eastern Africa.
+ >>>Does your primary language have any significant differences from English?
+Yes, there are certain words in English that do not exist in kiswahili. However,
+in the recent years experts of the swahili language have added words to fit the
+ever changing and evolving world. 
+>>>Any similarities to Guugu Yimithirr? Explain.
+Not at all. The compass directions and perceptions of space used in English
+happen to be very similar to those used in my native language. However this
+understanding I have may be influenced by the fact that I grew learning both English
+and swahili, hence kept translating them from one language to another.
+
+c.>>>Have there been moments in which you felt that your primary language or
+another language you speak "got in the way" of something you wanted to express?
+That is, have you run into the limitations of language in the manner suggested by
+the excerpt? Elaborate.
+I have had many experiences where Kiswahili got in the way of a conversation or
+an interaction with people from other countries. Often because of direct translation
+to english the meaning, detail, sensitivity or urgency of a message changes and is lost
+I recently discovered that I am a language enthusiast and more so of African Language.
+This interest has strengthened my relationship with friends from Rwanda, Zimbabwe,
+Nigeria and Ghana. Nonetheless, I realised that the more I learned the more I
+saw the differences between people and how they think. To a certain degree the language
+you speak also shapes how agressive, submissive or out going you become. Rwandans
+are more laid back, and calm and so is their language. Ghanaians on the other hand,
+speak a language that is demostrative and warm so they happen to be very
+outgoing.
+One thing to note is that these languages are not linear in effect to human thinking
+and behaviour. In contrast they are interconnected and affect each other.
+d.Discuss to what extent you think artificial languages (such as Python) have
+the ability to shape human thought, if at all. Or, do you think artificial
+languages are fundamentally different from human ones, such as Guugu Yimithirr
+or English? Elaborate.
+I do think artificial or formal languages, have rules and boundaries with which
+programmers have to stay within. While I understand that this is important for
+predicting desired outcomes, the consequence is that as a programmer my creativity
+may be limited. I may be forced to think within a box and not have the opportunity
+to expressed wild, daring imaginations. Artificial languages are fundamentally
+different from natural languages not heavily
+influenced by human behaviour in comparison to natural languages, this may mean
+that the interconnection present between artifical language and people, will be strictly
+linear. What does that mean in relation to human thought? It means that our thinking
+can and easily be influenced by these very languages.
+Our brains are very categorical, so if you feed it information that is within a certain
+boundary it will only always respond to that farmiliar information.
+
+e.Are there parts of the Python language that feel foreign to you, despite
+knowing what the word means in English? 
+So far many of the parts used in python describe what exactly they mean. I am yet
+to encounter a part that doesnt but as soon as I do, I will take note of it.
+
+The more I interact with python the complexity I once thought it had becomes simpler
+and easier to understand. One striking difference python has from english,
+is that python has a completely different structure that can be daunting at first.
+
+I can be able to figure out what x represents because I have learnt about variable
+names used in python and now I understand that letters such as x can be used to name
+and store different types of values.
+
+
+Is there supposed to be any danger when using nested loops
+
+'''
